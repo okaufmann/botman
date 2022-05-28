7e54871d929d1db28a9c551bae95646e45275b8d
Oliver Kaufmann <okaufmann91@gmail.com>
2022-05-28 09:57:11 +0200

    Add custom Matcher, curl_options, configuration file.

--- src/Drivers/Tests/FakeDriver.php
+++ src/Drivers/Tests/FakeDriver.php
@@ -176,6 +176,18 @@ class FakeDriver implements DriverInterface, VerifiesService
     }
 
     /**
+     * Send a typing indicator and wait for the given amount of seconds.
+     * @param IncomingMessage $matchingMessage
+     * @param float $seconds
+     * @return mixed
+     */
+    public function typesAndWaits(IncomingMessage $matchingMessage, float $seconds)
+    {
+        $this->types($matchingMessage);
+        usleep($seconds * 1000000);
+    }
+
+    /**
      * Returns true if types() has been called.
      *
      * @return bool
@@ -202,7 +214,7 @@ class FakeDriver implements DriverInterface, VerifiesService
     }
 
     /**
-     * @return bool
+     * @return bool|GenericEvent
      */
     public function hasMatchingEvent()
     {
